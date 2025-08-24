a7e61dd07ac6a80264de54235c97685166943934
Eziiquiel <eziquielsilva64@gmail.com>
2025-08-24 17:37:31 -0300

    Refactor: adding a few more tests

--- src/main/java/com/challenge/components_injections/ComponentsInjectionsApplication.java
+++ src/main/java/com/challenge/components_injections/ComponentsInjectionsApplication.java
@@ -22,13 +22,29 @@ public class ComponentsInjectionsApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		Order order = new Order(1034, 150.00, 20.0);
 		
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("========= TESTE 1 ===========");
+		Order order = new Order(1034, 150.00, 20.0);
 		double total = orderService.total(order);
 		
 		System.out.println("Pedido código: " + order.getCode());
-		System.out.printf("Valor total: %.2f", total);
+		System.out.printf("Valor total: %.2f%n", total);
+		
+		System.out.println("========= TESTE 2 ===========");
+		order = new Order(2282, 800.00, 10.0);
+		total = orderService.total(order);
+		
+		System.out.println("Pedido código: " + order.getCode());
+		System.out.printf("Valor total: %.2f%n", total);
+		
+		System.out.println("========= TESTE 3 ===========");
+		order = new Order(1309, 95.90, 0.0);
+		total = orderService.total(order);
+		
+		System.out.println("Pedido código: " + order.getCode());
+		System.out.printf("Valor total: %.2f%n", total);
 		
 	}
 
